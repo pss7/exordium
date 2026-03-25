7604b8897072b5e18ddd1e24dce2d899d7f74cfe
parksangseok <park0157@naver.com>
2026-03-25 14:13:04 +0900

    commit

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ $(function () {
 
   /* section04 */
   $('#section04 .slickWrap .slick').slick({
+    slide: '.slider',
     autoplay: true,
     arrows: true,
     dots: false,
@@ -18,9 +19,10 @@ $(function () {
     prevArrow: $('#section04 .control .prev'),
     nextArrow: $('#section04 .control .next'),
   });
- 
-  /* section07 */
-  $('#section07 .slickWrap .slick').slick({
+
+  /* section06 */
+  $('#section06 .slickWrap .slick').slick({
+    slide: '.slider',
     autoplay: true,
     arrows: true,
     dots: false,
@@ -34,20 +36,211 @@ $(function () {
     pauseOnHover: false,
     autoplaySpeed: 3000,
     speed: 1300,
-    prevArrow: $('#section07 .control .prev'),
-    nextArrow: $('#section07 .control .next'),
+    prevArrow: $('#section06 .control .prev'),
+    nextArrow: $('#section06 .control .next'),
   });
- 
 
 
+  var sec1Open = false;
+  var sec3Open = false;
+  var sec6Open = false;
+  var busy = false;
+  var current = 1;
+  var firstLoad = true;
 
+  function getSectionId(index) {
+    return '#section' + ('0' + index).slice(-2);
+  }
 
+  function setActiveSection(index) {
+    $('.section').removeClass('is-active');
+    $(getSectionId(index)).addClass('is-active');
+  }
 
+  function resetSection03() {
+    $('#section03').removeClass('step1 horizontalStep2');
+    sec3Open = false;
+  }
 
+  $('#fullpage').fullpage({
+    scrollingSpeed: 2000,
+    navigation: false,
+    fitToSection: true,
+    autoScrolling: true,
+    slideSelector: '.fpSlide',
 
+    afterRender: function () {
+      setTimeout(function () {
+        setActiveSection(1);
+        firstLoad = false;
+      }, 100);
+    },
 
+    afterLoad: function (anchorLink, index) {
+      current = index;
 
+      if (!firstLoad) {
+        setTimeout(function () {
+          setActiveSection(index);
+        }, 50);
+      }
 
+      // section03 첫 화면 진입 시 step1 부여
+      if (index == 3 && !sec3Open) {
+        setTimeout(function () {
+          $('#section03').addClass('step1');
+        }, 150);
+      }
+    },
 
+    onLeave: function (index, nextIndex, direction) {
+      if (busy) return false;
+
+      /* section01 */
+      if (index == 1 && direction == 'down' && !sec1Open) {
+        busy = true;
+        $('#section01').addClass('on2');
+        sec1Open = true;
+        setActiveSection(1);
+
+        setTimeout(function () {
+          busy = false;
+        }, 800);
+
+        return false;
+      }
+
+      if (nextIndex == 1) {
+        $('#section01').removeClass('on2');
+        sec1Open = false;
+      }
+
+      /* section03 */
+      if (index == 3 && direction == 'down' && !sec3Open) {
+        busy = true;
+        $('#section03').removeClass('step1').addClass('horizontalStep2');
+        sec3Open = true;
+        setActiveSection(3);
+
+        setTimeout(function () {
+          busy = false;
+        }, 800);
+
+        return false;
+      }
+
+      if (index == 4 && nextIndex == 3 && direction == 'up') {
+        $('#section03').removeClass('step1').addClass('horizontalStep2');
+        sec3Open = true;
+
+        setTimeout(function () {
+          setActiveSection(3);
+        }, 50);
+      }
+
+      if (index == 2 && nextIndex == 3 && direction == 'down') {
+        resetSection03();
+
+        setTimeout(function () {
+          $('#section03').addClass('step1');
+          setActiveSection(3);
+        }, 150);
+      }
+
+      /* section06 */
+      if (index == 6 && direction == 'down' && !sec6Open) {
+        busy = true;
+        $('#section06').addClass('horizontalStep2');
+        sec6Open = true;
+        setActiveSection(6);
+
+        setTimeout(function () {
+          $('#section06 .slick').slick('setPosition');
+          busy = false;
+        }, 800);
+
+        return false;
+      }
+
+      if (index == 7 && nextIndex == 6 && direction == 'up') {
+        $('#section06').addClass('horizontalStep2');
+        sec6Open = true;
+
+        setTimeout(function () {
+          $('#section06 .slick').slick('setPosition');
+          setActiveSection(6);
+        }, 50);
+      }
+
+      if (index == 5 && nextIndex == 6 && direction == 'down') {
+        $('#section06').removeClass('horizontalStep2');
+        sec6Open = false;
+
+        setTimeout(function () {
+          $('#section06 .slick').slick('setPosition');
+          setActiveSection(6);
+        }, 50);
+      }
+    }
+  });
+
+  $('#section01').on('mousewheel DOMMouseScroll', function (e) {
+    if (current != 1 || !sec1Open || busy) return;
+
+    var delta = e.originalEvent.wheelDelta || -e.originalEvent.detail;
+
+    if (delta > 0) {
+      busy = true;
+      $('#section01').removeClass('on2');
+      sec1Open = false;
+      setActiveSection(1);
+
+      setTimeout(function () {
+        busy = false;
+      }, 800);
+
+      return false;
+    }
+  });
+
+  $('#section03').on('mousewheel DOMMouseScroll', function (e) {
+    if (current != 3 || !sec3Open || busy) return;
+
+    var delta = e.originalEvent.wheelDelta || -e.originalEvent.detail;
+
+    if (delta > 0) {
+      busy = true;
+      $('#section03').removeClass('horizontalStep2');
+      sec3Open = false;
+      setActiveSection(3);
+
+      setTimeout(function () {
+        $('#section03').addClass('step1');
+        busy = false;
+      }, 150);
+
+      return false;
+    }
+  });
+
+  $('#section06').on('mousewheel DOMMouseScroll', function (e) {
+    if (current != 6 || !sec6Open || busy) return;
+
+    var delta = e.originalEvent.wheelDelta || -e.originalEvent.detail;
+
+    if (delta > 0) {
+      busy = true;
+      $('#section06').removeClass('horizontalStep2');
+      sec6Open = false;
+      setActiveSection(6);
+
+      setTimeout(function () {
+        $('#section06 .slick').slick('setPosition');
+        busy = false;
+      }, 800);
+
+      return false;
+    }
+  });
 
 });
