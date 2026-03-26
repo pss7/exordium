3e13f129c771ac0edce6318f18e7d3f5ac9f46bf
parksangseok <park0157@naver.com>
2026-03-26 18:46:26 +0900

    commit

--- js/sub.js
+++ js/sub.js
@@ -1,31 +1,45 @@
 
 $(function () {
 
-
-
-  $(window).on('load', function () {
+  //로드
+  $(window).load(function () {
     $('.subTopTitleBox').addClass('active');
   });
 
-$('.overviewMainimg').slick({
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  arrows: false,
-  asNavFor: '.overviewNavImg'
-});
+  //사업개요
+  $('.overviewMainimg').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    asNavFor: '.overviewNavImg'
+  });
 
-$('.overviewNavImg').slick({
-  arrows: false,
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  infinite: true,
-  asNavFor: '.overviewMainimg',
-  focusOnSelect: true,
-  variableWidth: true,
-  swipeToSlide: true
-});
+  $('.overviewNavImg').slick({
+    arrows: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    infinite: true,
+    asNavFor: '.overviewMainimg',
+    focusOnSelect: true,
+    variableWidth: true,
+    swipeToSlide: true
+  });
 
+  //탭
+  $('.tabContent').hide();
+  $('.tabContent').first().show();
 
+  $('.tabList li').click(function () {
+
+    $('.tabList li .tabBtn').removeClass('active');
+    $(this).children('.tabBtn').addClass('active')
+
+    let idx = $(this).index();
+
+    $('.tabContent').hide();
+    $('.tabContent').eq(idx).show();
+
+  });
 
 
 });
