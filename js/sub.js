08beef59627f051703a9132264d6060beaf1aa08
parksangseok <park0157@naver.com>
2026-03-26 17:47:21 +0900

    commit

--- js/sub.js
+++ js/sub.js
@@ -7,18 +7,23 @@ $(function () {
     $('.subTopTitleBox').addClass('active');
   });
 
-  // $('.overviewMainimg').slick({
-  //   slidesToShow: 1,
-  //   slidesToScroll: 1,
-  //   arrows: false,
-  //   asNavFor: '.overviewNavImg'
-  // });
-  // $('.overviewNavImg').slick({
-  //   slidesToShow: 4,
-  //   slidesToScroll: 1,
-  //   asNavFor: '.overviewMainimg',
-  //   focusOnSelect: true
-  // });
+$('.overviewMainimg').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  asNavFor: '.overviewNavImg'
+});
+
+$('.overviewNavImg').slick({
+  arrows: false,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  infinite: true,
+  asNavFor: '.overviewMainimg',
+  focusOnSelect: true,
+  variableWidth: true,
+  swipeToSlide: true
+});
 
 
 
